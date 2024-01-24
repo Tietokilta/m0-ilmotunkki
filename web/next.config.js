29e577e7e1a76b844bc2b6f225d440e15dc751bd
Ahlström Kalle <kalle.ahlstrom@iki.fi>
2024-01-24 03:09:54 +0200

    add *.blob.core.windows.net to remotePatterns

--- web/next.config.js
+++ web/next.config.js
@@ -5,23 +5,27 @@ const nextConfig = {
     loader: "default",
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
+        protocol: "http",
+        hostname: "localhost",
       },
       {
-        protocol: 'https',
-        hostname: '*.paytrail.com',
+        protocol: "https",
+        hostname: "*.paytrail.com",
       },
       {
-        protocol: 'http',
-        hostname: 'cms',
-      }
+        protocol: "http",
+        hostname: "cms",
+      },
+      {
+        protocol: "https",
+        hostname: "*.blob.core.windows.net",
+      }, // TODO: if we ever need to use images from other external domains, add them here
     ],
   },
   eslint: {
     dirs: ["middleware.ts", "app", "context", "components"],
   },
   output: "standalone",
-}
+};
 
 module.exports = nextConfig;
