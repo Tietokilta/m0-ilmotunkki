d417308cf0490a3b16cf439250f09e405178a295
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-01-25 15:30:10 +0200

    make cloud storage be ignored in dev

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -4,22 +4,28 @@ const useCloudStorage =
   typeof process.env.STORAGE_URL === "string" &&
   typeof process.env.STORAGE_CONTAINER_NAME === "string";
 
-export default ({ env }) => ({
+const plugins = ({ env }) => ({
   management: {
     enabled: true,
     resolve: "./src/plugins/management",
   },
-  upload: {
-    enabled: useCloudStorage,
-    config: {
-      provider: "strapi-provider-upload-azure-storage",
-      providerOptions: {
-        account: env("STORAGE_ACCOUNT"),
-        accountKey: env("STORAGE_ACCOUNT_KEY"), //either account key or sas token is enough to make authentication
-        serviceBaseURL: env("STORAGE_URL"), // optional
-        containerName: env("STORAGE_CONTAINER_NAME"),
-        defaultPath: "uploads",
-      },
-    },
-  },
+  ...(useCloudStorage
+    ? {
+        upload: {
+          enabled: useCloudStorage,
+          config: {
+            provider: "strapi-provider-upload-azure-storage",
+            providerOptions: {
+              account: env("STORAGE_ACCOUNT", ""),
+              accountKey: env("STORAGE_ACCOUNT_KEY", ""), //either account key or sas token is enough to make authentication
+              serviceBaseURL: env("STORAGE_URL", ""), // optional
+              containerName: env("STORAGE_CONTAINER_NAME", ""),
+              defaultPath: "uploads",
+            },
+          },
+        },
+      }
+    : {}),
 });
+
+export default plugins;
